cc08aa2f16874c891b0cf61abc2e0b5bd54de489
Leonardo Sousa <leonardolsousa5@gmail.com>
2021-02-23 10:39:08 -0300

    Criando BD com typeorm e controller, model e endpoint

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,29 +1,12 @@
+import 'reflect-metadata';
 import express from 'express';
+import "./database";
+import { router } from "./routes";
 
 const app = express();
 
-/**
- * GET => Busca
- * POST => Salvar
- * PUT => Alterar
- * DELETE => Deletar
- * PATCH => Alteração específica
- */
-
-
-//http://localhost:3333/users
- app.get("/", (request, response) => {
-     return response.json({ message: "Hello Next Level Week #4" })
- });
-
-
- // 1 param => Rota(Recurso dentro da API)
- // 2 param => request, response
-
- app.post("/", (request, response) => {
-     //Recebeu os dados para salvar
-     return response.json({ message: "Os dados foram salvos com sucesso!" })
- });
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log('Server is running'));
 
