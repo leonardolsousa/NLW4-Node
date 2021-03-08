4f39b57969125b6d220ed20e704050b97b72e91a
Leonardo Sousa <leonardolsousa5@gmail.com>
2021-03-07 21:07:41 -0300

    Teste de Users com email duplicado

--- api/src/__tests__/User.test.ts
+++ api/src/__tests__/User.test.ts
@@ -1,11 +1,34 @@
 import request from 'supertest';
-import { app } from '../app'
+import { app } from '../app';
+
+import createConnection from '../database';
 
 describe("Users", () => {
+    beforeAll(async () => {
+        const connection = await createConnection();
+        await connection.runMigrations();
+    });
 
-    request(app).post("/users")
-        .send({
-            email: "user@example.com",
-            name: "User Example"
-        })
-})
+
+    it("Should be able to create new user", async () => {
+
+        const response = await request(app).post("/users")
+            .send({
+                email: "user@example.com",
+                name: "User Example",
+            });
+
+        expect(response.status).toBe(201);
+    });
+
+    it("Should not to be able to create a user with exists email", async () => {
+
+        const response = await request(app).post("/users")
+            .send({
+                email: "user@example.com",
+                name: "User Example",
+            });
+
+        expect(response.status).toBe(400);
+    });
+});
--- api/src/database/index.ts
+++ api/src/database/index.ts
@@ -1,6 +1,13 @@
-import { Connection, createConnection } from "typeorm";
+import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
 export default async (): Promise<Connection> => {
+    const defaultOptions = await getConnectionOptions();
 
-    return createConnection()
-}
+    return createConnection(
+        Object.assign(defaultOptions, {
+            database: process.env.NODE_ENV === "test"
+                ? "./src/database/database.test.sqlite"
+                : defaultOptions.database,
+        })
+    );
+};
